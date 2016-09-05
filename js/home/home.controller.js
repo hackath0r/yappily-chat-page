9d6211af503d8e35d2676d8669350e7719b58e45
Garima Kamboj <garima@koove.com>
2016-09-06 01:16:43 +0530

    Project Done Without Style

---
+++ js/home/home.controller.js
@@ -0,0 +1,22 @@
+(function(){
+	angular
+		.module('home')
+		.controller('HomeCtrl', function($http, $scope){
+			$scope.currentLoggedInUser = {
+				'name': 'Garima Kamboj',
+				'profile_pic': 'http://placehold.it/150?text=Garima'
+			}
+			$scope.query = '';
+			$http.get('data/userList.json')
+				 .then(function(response){
+				 	$scope.userList = response.data;
+				 });
+			$scope.showChat = function(user) {
+				$scope.selectedUser = user;
+				$http.get('data/conv.json')
+				 .then(function(response){
+				 	$scope.conv = response.data;
+				 });
+			}
+		});
+})();
